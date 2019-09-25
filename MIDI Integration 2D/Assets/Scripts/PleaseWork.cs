fbe04083be26eb0138375445bdc9ed7fed8dbc3b
MSI\cyber <darkforcesii@gmail.com>
2019-09-24 20:51:39 -0700

    Please Work

---
+++ MIDI Integration 2D/Assets/Scripts/PleaseWork.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+public class PleaseWork : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MidiDriver.Instance.GetKey(MidiChannel.All,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+
+
+            if (MidiDriver.Instance.GetKey(MidiChannel.All, i) > 0)
+            {
+                print("please work"+ i);
+
+            }
+        }
+    }
+
+}
