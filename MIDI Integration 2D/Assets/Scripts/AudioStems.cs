f3b53c529074b403b307eb42202b02abf49c0d5f
MSI\cyber <darkforcesii@gmail.com>
2019-09-24 18:38:08 -0700

    Added audio, scripts, and main menu
    Added audio that plays during main menu
    Added scripts that allow audio to play based on UI
    Added main menu and functionality to transition between scenes

---
+++ MIDI Integration 2D/Assets/Scripts/AudioStems.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStems : MonoBehaviour
+{
+
+    AudioSource mainAudio;
+    public AudioClip bassLine;
+    public AudioClip chords;
+    public AudioClip MenuSelect;
+    public AudioClip melody;
+    public AudioClip strings;
+    public AudioClip ModeSelect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Chords());
+        StartCoroutine(Melody());
+        StartCoroutine(BassLine());
+    }
+
+    public void OnMouseClick()
+    {
+        GetComponent<AudioSource>().PlayOneShot(MenuSelect);
+    }
+
+    public void OnMouseEnter()
+    {
+        GetComponent<AudioSource>().PlayOneShot(MenuSelect);
+    }
+
+    IEnumerator Chords()
+    {
+        while (true)
+        {
+            GetComponent<AudioSource>().PlayOneShot(chords);
+            yield return new WaitForSecondsRealtime(27.428f);
+        }
+    }
+
+    IEnumerator BassLine()
+    {
+        if (Random.value > 0.2)
+        {
+            while (true)
+            {
+                GetComponent<AudioSource>().PlayOneShot(bassLine);
+            }
+        }
+        yield return new WaitForSecondsRealtime(27.428f);
+    }
+
+    IEnumerator Melody()
+    {
+        while (true)
+        {
+            if(Random.value > 0.5)
+            {
+                GetComponent<AudioSource>().PlayOneShot(melody);
+            }
+            yield return new WaitForSecondsRealtime(27.428f);
+        }
+    }
+
+    IEnumerator Strings()
+    {
+        if (Random.value > 0.7)
+        {
+            GetComponent<AudioSource>().PlayOneShot(strings);
+        }
+        yield return new WaitForSecondsRealtime(27.428f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MIDI Integration 2D/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+    }
+
+    public void OnLevelChange()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
