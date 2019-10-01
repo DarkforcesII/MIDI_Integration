20d0b999cff5bb7da1b78598651981c453c1a5af
MSI\cyber <darkforcesii@gmail.com>
2019-10-01 16:20:21 -0700

    Added MicScript
    Added script to see how viable mic support in Unity is.

---
+++ MIDI Integration 2D/Assets/Scripts/MicScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicScript : MonoBehaviour
+{
+    public static string[] devices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get list of Microphone Devices
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name:" + device);
+        }
+
+        // Start recording with built in Microphone and play recorded audio right away
+        AudioSource micInput = GetComponent<AudioSource>();
+        micInput.clip = Microphone.Start("Realtek High Definition Audio", true, 10, 44100);
+        /*micInput.Play();*/
+        /*GetComponent<AudioSource>().PlayOneShot(AudioClip);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
